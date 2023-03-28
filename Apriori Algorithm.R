137040f2e8f35dfd9065bf7406fb6a8a2a17df74
Satya Ajay <86813260+rsa914016@users.noreply.github.com>
2023-03-28 20:55:00 +0530

    Update Apriori Algorithm.R

--- Apriori Algorithm.R
+++ Apriori Algorithm.R
@@ -1,18 +1,13 @@
-# Loading Libraries
 library(arules)
 library(RColorBrewer)
 
-# import dataset
 data("Groceries")
 
-# using apriori() function
 rules <- apriori(Groceries,
                  parameter = list(supp = 0.01, conf = 0.2))
 
-# using inspect() function
 inspect(rules[1:5])
 
-# using itemFrequencyPlot() function
 arules::itemFrequencyPlot(Groceries, topN = 7,
                           col = brewer.pal(8, 'Pastel2'),
                           main = 'Relative Item Frequency Plot',
